5efd5f2c78a9e92357dd91d698adf2d34b79e2ca
prabhattechnohunk <prabhatproject1@gmail.com>
2026-06-09 20:05:59 +0530

    comment added in develop2

--- src/main/java/org/example/App.java
+++ src/main/java/org/example/App.java
@@ -17,5 +17,6 @@ public class App
         System.out.println(socket);
         System.out.println("Changes by Salim");
        Hello.hello("Bijaya" ,101);
+       //modification in prabhat-deveop2
     }
 }
