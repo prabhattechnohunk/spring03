59a9c060d6b6571af00df286c840704d5f06d1ef
bijayakc <bkc8@uco.edu>
2026-06-09 23:05:11 -0500

    multiply done

--- src/main/java/org/example/Hello.java
+++ src/main/java/org/example/Hello.java
@@ -1,9 +1,14 @@
 package org.example;
 
 public class Hello {
-    static void hello(String fname , int id){
-        System.out.println("hello"+" "+ fname +" your employee id is "+ id);
+    static void hello(String fname, int id) {
+        System.out.println("hello" + " " + fname + " your employee id is " + id);
     }
 
+
+    public static int multiply(int a , int b , int c)
+    {
+        return a*b*c;
+    }
 
 }
